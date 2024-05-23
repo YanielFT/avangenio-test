219ce57916882582d56a1c80141578203a91b09f
YanielFT <ceyfuentes@gmail.com>
2024-05-23 16:38:03 -0500

    Bento

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { TopMenu } from "./ui/top-menu/topMenu";
+export { Bento } from "./ui/bento/bento";
+
+
---
+++ src/components/ui/bento/bento.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import React from "react";
+
+export const Bento = () => {
+  return (
+    <div className="flex justify-between items-center mt-32 mb-32">
+      <div className="sm:basis-[55%] rounded-md overflow-hidden">
+        <Image
+          className="object-cover w-full h-[14rem] sm:h-[22rem]"
+          src="/landscape1.webp"
+          alt="landscape1"
+          width={250}
+          height={250}
+        />
+      </div>
+      <div className="sm:basis-[44%] sm:block rounded-md overflow-hidden hidden">
+        <Image
+          className="object-cover w-full h-[22rem]"
+          src="/landscape2.webp"
+          alt="landscape2"
+          width={250}
+          height={250}
+        />
+      </div>
+    </div>
+  );
+};
